2c7c760f12713bad4a96fbbbb5ff138df0d4b7a9
André Bueno <andre_devops@hotmail.com>
2026-01-09 15:50:55 -0300

    bash

---
+++ bash_n8n_aws_easypanel.sh
@@ -0,0 +1,13 @@
+sudo -i
+
+curl -sSL https://get.easypanel.io | sh
+
+# usar o link gerado para acessar o esaypanel
+
+# configurar a porta 3000 (TCP) no security group para acessar o easypanel
+
+# no easypanel procurar n8n para ser instalardo
+
+# adicionar regra https para acessar o n8n
+
+# usar o link gerado na instalação do n8n.
