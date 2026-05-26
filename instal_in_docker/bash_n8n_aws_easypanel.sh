adf3d9cea3621d0f32ae615119455f61a360372c
André Bueno <andre_devops@hotmail.com>
2026-05-26 16:04:09 -0300

    waha_n8n

--- instal_in_docker/bash_n8n_aws_easypanel.sh
+++ instal_in_docker/bash_n8n_aws_easypanel.sh
@@ -15,3 +15,38 @@ curl -sSL https://get.easypanel.io | sh
 # usar o link gerado na instalação do n8n.
 
 versão a ser usada 2.18.5
+
+
+WAHA_API_KEY=sua_chave_super_secreta
+WAHA_DASHBOARD_USERNAME=admin
+WAHA_DASHBOARD_PASSWORD=sua_senha_segura
+WHATSAPP_SWAGGER_USERNAME=admin
+WHATSAPP_SWAGGER_PASSWORD=sua_senha_segura
+WAHA_DASHBOARD_ENABLED=True
+WHATSAPP_SWAGGER_ENABLED=True
+WHATSAPP_DEFAULT_ENGINE=WEBJS
+
+
+1-  Vídeo ✅
+Transforme Seu PC em um Servidor Poderoso com EasyPanel (Passo a Passo!) 🚀
+https://www.youtube.com/watch?v=4S3V3dIYJ34
+
+2 - Vídeo ✅
+Instalando n8n no Servidor Caseiro com EasyPanel (Passo a Passo!) 🚀
+https://www.youtube.com/watch?v=s5LE-4lW1qk
+
+3 - Vídeo ✅
+Instalando EvolutionAPI no Servidor Caseiro com EasyPanel (Passo a Passo!) 🚀
+https://www.youtube.com/watch?v=Ag8vGGzmiwc
+
+4 - Vídeo ✅
+Instalando Waha no Servidor Caseiro com EasyPanel (Passo a Passo!) 🚀
+https://www.youtube.com/watch?v=Ms-6H_7Wuc4
+
+5 - Vídeo ✅
+Como Publicar Servidor caseiro na Internet (Passo a Passo!) 🚀
+https://www.youtube.com/watch?v=MHAoIUmfzO0
+
+6 - waha com n8n
+https://www.youtube.com/watch?v=qW7pSrha_2A
+
